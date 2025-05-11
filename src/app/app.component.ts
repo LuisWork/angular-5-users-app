dc8eddf23c4f42ea521de0dc1cf53ca54a09f628
LuisWork <luiszambrano.work@gmail.com>
2025-05-11 17:28:56 -0400

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserAppComponent } from './components/user-app/user-app.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/user-app/user-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-app',
+  imports: [],
+  templateUrl: './user-app.component.html',
+  styleUrl: './user-app.component.css'
+})
+export class UserAppComponent {
+
+  title: String = 'Angular 5 Users App';
+
+}
