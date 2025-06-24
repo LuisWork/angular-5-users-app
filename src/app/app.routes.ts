31f344ba5e19e9064e48816c83a776ebd0921d0d
LuisWork <luiszambrano.work@gmail.com>
2025-06-24 12:56:03 -0400

    Implementando Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/users/page/0' },
     { path: 'users', component: UserComponent },
     { path: 'users/page/:page', component: UserComponent },
-    { path: 'users/create', component: UserFormComponent },
-    { path: 'users/edit/:id', component: UserFormComponent },
+    { path: 'users/create', component: UserFormComponent, canActivate: [authGuard]  },
+    { path: 'users/edit/:id', component: UserFormComponent, canActivate: [authGuard] },
     { path: 'login', component: AuthComponent },
 ];
