105bdad74e242865390f1a42b1c49eff1c7b3397
LuisWork <luiszambrano.work@gmail.com>
2025-07-02 15:14:47 -0400

    Implementando Reducers a los metodos del CRUD

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
 import { provideStore } from '@ngrx/store';
+import { usersReducer } from './store/users.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
-    provideStore()
+    provideStore({
+      users: usersReducer
+    })
 ]
 };
