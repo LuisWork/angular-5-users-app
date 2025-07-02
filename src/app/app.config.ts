f5d4680a48b7cdf778f712ca7c5c120320225ced
LuisWork <luiszambrano.work@gmail.com>
2025-07-02 14:52:53 -0400

    Instalando Ngrx e implementando users actions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideStore()
+]
 };
